2937de3b5983dcc74bce9bf19bc733168e987ccd
lukehardcastle12 <lukehardcastle12@gmail.com>
2023-06-14 13:37:03 +0100

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(input, repeated){
+    let output = "";
+    if(repeated>=0){
+        for(i=1;i<=repeated;i++){
+            output = output + input
+        }
+        return output;
+    }
+    else{
+        return "ERROR";
+    }
 
+    
 };
 
 // Do not edit below this line
