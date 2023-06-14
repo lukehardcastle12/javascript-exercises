7476ef5916e112d5220ee3d9a7e9a66d9d2f7deb
lukehardcastle12 <lukehardcastle12@gmail.com>
2023-06-14 13:47:02 +0100

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed=""
+    for(i=0;i<=string.length;i++){
+        reversed += string.charAt(string.length-i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
