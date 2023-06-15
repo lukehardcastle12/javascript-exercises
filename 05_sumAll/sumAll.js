319cbf5d134ad0a8ee1ec4225dad80713f82235a
lukehardcastle12 <lukehardcastle12@gmail.com>
2023-06-15 15:20:32 +0100

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+//take two numbers
+//sum all numbers between the two numbers
+//return the sum
+//1,4
+//starts as 1 + 2 + 3 + 4 = 10
+//
+const sumAll = function(start, end) {
+    let finalNum;
+    //error for negative number
+    if(start < 0 || end < 0){
+        return 'ERROR';
+    }
+    // error if input is not a number
+    if(isNaN(start) || isNaN(end) || typeof start  === 'string' || typeof end === 'string'){
+        return 'ERROR';
+    }
+    //smaller first
+    if(start < end){
+        let finalNum = start;
+        for(i=start + 1;i <= end; i++){
+            finalNum += i;
+        }
+        return finalNum;
+    }
+    //bigger first
+    if(start>end){
+        let finalNum = end;
+        for(i=end + 1;i <= start; i++){
+            finalNum += i;
+        }
+        return finalNum;
+    }
 
 };
 
